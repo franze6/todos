06ee0147769659e867f02b5206211145ec89c90e
Kirill A. Artemovich <kandrusyak@tsconsulting.com>
2020-07-11 15:46:50 +0300

    edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: "./src/index.js",
